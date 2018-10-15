2aad1160e3286c19767ca7bb7c479f47a4224e66
raauger <raaplace@gmail.com>
2018-10-15 11:23:08 -0400

    draft reorganizing race classes

--- src/main/java/edu/bsu/cs222/Race.java
+++ src/main/java/edu/bsu/cs222/Race.java
@@ -3,41 +3,31 @@ package edu.bsu.cs222;
 import java.util.ArrayList;
 
 public class Race {
-    private String raceName;
-    private ArrayList<Trait> raceTraits;
-    private Integer traitBonus; //Will need to be manually set for each race.
-    private String traitName; //Will need to be manually set for each race. This is the trait that the trait bonus is applied to.
-    private String raceDescription; //Will need to be manually set for each race.
+    private String name;
+    private String description; //Will need to be manually set for each race.
+    private ArrayList<traitBonus> traitBonus;
 
     /*
     Passing all arguments through the constructor because Races are static. We will need to manually create each Race that
-    users are able to choose from. These Races will have preset names, traitBonuses, traits, and descriptions.
+    users are able to choose from. These Races will have preset names, traitBonuses, traitNames, and descriptions.
      */
 
-    public Race(String raceName,String traitName,Integer traitBonus, String raceDescription){
-        this.raceName = raceName;
+    public Race(String name,ArrayList<traitBonus> traitBonus, String description){
+        this.name = name;
         this.traitBonus = traitBonus;
-        this.raceDescription = raceDescription;
-        this.traitName = traitName;
+        this.description = description;
     }
 
-    /*
-    public void setTrait(Trait trait){
-        this.trait = trait;
-    }
-    */
-
-    public String getTraitName(){
-        return this.traitName;
-    }
-
-    public Integer getTraitBonus(){
+    public ArrayList<traitBonus> getTraitBonuses(){
         return this.traitBonus;
     }
 
-
     public String toString(){
-        return("Race : " + raceName + "|\n" + raceDescription + "\n\t , Adds " + traitBonus + " to " + traitName + ".");
+        String result = "Race : "+this.name+"\n\tDescription : "+this.description;
+        for(int i=0;i<traitBonus.size();i++){
+            result += "\n\tAdds "+traitBonus.get(i).getTraitBonusValue()+" to "+traitBonus.get(i).getTraitName();
+        }
+        return result;
     }
 
 }
